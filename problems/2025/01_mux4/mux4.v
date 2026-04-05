d2e60e4c6f463b051ebbd1984c346c004ecad9c7
ralex2304 <ralex2304@gmail.com>
2026-04-05 20:34:09 +0300

    [problems] fix 2025.01: mux4 - remove multidimensional port

--- problems/2025/01_mux4/mux4.v
+++ problems/2025/01_mux4/mux4.v
@@ -2,11 +2,20 @@ module mux4 #(
     parameter DATA_WIDTH = 32
 )(
     input  wire                 [1:0] i_select,
-    input  wire [3:0][DATA_WIDTH-1:0] i_data,
+    input  wire    [4*DATA_WIDTH-1:0] i_data,
 
     output wire      [DATA_WIDTH-1:0] o_data
 );
 
-assign o_data = i_data[i_select];
+wire [DATA_WIDTH-1:0] data [3:0];
+
+generate
+genvar i;
+for (i = 0; i < 4; i = i + 1) begin : genDataArr
+	assign data[i] = i_data[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH];
+end
+endgenerate
+
+assign o_data = data[i_select];
 
 endmodule
